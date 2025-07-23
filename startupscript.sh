6b3bebb01eef4c2b17a16a6e21c3f44d2e17d74f
katkarvishalen99 <121720944+katkarvishalen99@users.noreply.github.com>
2025-07-23 18:42:45 +0530

    Update startupscript.sh

--- startupscript.sh
+++ startupscript.sh
@@ -4,4 +4,4 @@ sudo apt-get install -y apache2
 systemctl start apache2
 systemctl enable apache2
 #echo "<html><body>Hello, from ${hostname} server </body></html>" > /var/www/html/index.html
-#curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh && sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh && sudo bash add-google-cloud-ops-agent-repo.sh --also-install
